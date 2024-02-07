6c38b97671c5a3f0f39e7a6c8d3bde502eb80af6
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2024-02-07 08:08:33 -0500

    Added windows stuff

--- build.py
+++ build.py
@@ -39,24 +39,38 @@ def main():
 
   pyembedded_dir = os.path.join(os.path.dirname(__file__), 'pyembedded')
   if not os.path.exists(pyembedded_dir):
-    subprocess.run('pyoxidizer generate-python-embedding-artifacts'.split() + [ pyembedded_dir ])
+    subprocess.run('pyoxidizer generate-python-embedding-artifacts'.split() + [ pyembedded_dir ], check=True)
 
   # Perform the build
   os.environ['PYO3_CONFIG_FILE'] = os.path.join(pyembedded_dir, 'pyo3-build-config-file.txt')
 
-  subprocess.run('cargo build --release'.split())
+  if os.name == 'nt':
+    subprocess.run('cargo build --release --target=x86_64-pc-windows-msvc'.split())
+  else:
+    subprocess.run('cargo build --release --target=x86_64-unknown-linux-gnu'.split())
 
   print()
   print('Done building!')
   print()
 
   if not os.name == 'nt' and shutil.which('ldd'):
-    print('target/release/tacit-py')
-    subprocess.run('ldd target/release/tacit-py'.split())
+    print('target/x86_64-unknown-linux-gnu/release/tacit-py')
+    subprocess.run('ldd target/x86_64-unknown-linux-gnu/release/tacit-py'.split())
+  elif os.name == 'nt':
+    possible_dumpbin_locations = [
+      r'C:\Program Files\Microsoft Visual Studio\2022\VC\Tools\MSVC\14.37.32822\bin\Hostx64\x64\dumpbin.exe'
+    ]
+    for possible_location in possible_dumpbin_locations:
+      if os.path.exists(possible_location):
+        subprocess.run([possible_location, '/DEPENDENTS', r'target\x86_64-pc-windows-msvc\release\tacit-py.exe'])
+        break
 
   print()
   print('Running..')
-  subprocess.run('cargo run --release'.split())
+  if os.name == 'nt':
+    subprocess.run('cargo run --release --target=x86_64-pc-windows-msvc'.split())
+  else:
+    subprocess.run('cargo run --release --target=x86_64-unknown-linux-gnu'.split())
 
 
 
