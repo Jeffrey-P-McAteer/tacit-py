47246e64c912003a44662a832366952706c66b2a
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-06 20:07:15 -0500

    egui experiment, no native dependencies!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+
+use eframe::egui;
+
 use pyo3::prelude::*;
 
 static PYEMBEDDED_STDLIB_DIR: include_dir::Dir<'_> = include_dir::include_dir!("pyembedded/stdlib");
@@ -19,6 +22,24 @@ fn main() {
   }
   extract_children(&PYEMBEDDED_STDLIB_DIR, &pyembedded_stdlib);
   std::env::set_var("PYTHONPATH", pyembedded_stdlib.into_os_string() );
+
+  // GUI experiment
+  {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Box::<MyApp>::default()
+        }),
+    );
+  }
 
 
   let r = {
@@ -84,3 +105,45 @@ fn hide_console_on_windows_win() {
     }
     // Otherwise do nothing, we want console messages when run from the console.
 }
+
+
+
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            /*ui.image(egui::include_image!(
+                "../../../crates/egui/assets/ferris.png"
+            ));*/
+
+        });
+    }
+}
+
+
